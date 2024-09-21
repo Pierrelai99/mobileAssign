ea0753a71b365d36d8cdc614dcb7f8bfaf2cea87
Pierrelai99 <laijinhui99@gmail.com>
2024-09-21 22:43:27 +0800

    Brief description of changes made

---
+++ app/src/main/java/com/example/assgntmobile0921/database/AppDatabase.kt
@@ -0,0 +1,44 @@
+package com.example.assgntmobile0921.database
+
+import android.content.Context
+import androidx.databinding.adapters.Converters
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [User::class, Seller::class, FoodList::class, Order::class, OrderList::class, Payment::class, PaymentDetails::class],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
+    abstract fun sellerDao(): SellerDao
+    abstract fun foodListDao(): FoodListDao
+    abstract fun orderDao(): OrderDao
+    abstract fun orderListDao(): OrderListDao
+    abstract fun paymentDao(): PaymentDao
+    abstract fun paymentDetailsDao(): PaymentDetailsDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                )
+                    .createFromAsset("database/sql_basics.db")
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
